d03e2e7c37a75584cf9018c1a71b7641f2598349
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-01-15 10:54:34 +0100

    feat: useState for useBlog

--- modules/blog/composables/useBlog.ts
+++ modules/blog/composables/useBlog.ts
@@ -1,5 +1,9 @@
+import { useState } from "#app"
+
 export const useBlog = () => {
+  const blogPostId = useState('blog-post-id', () => 1)
+  
   return {
-    blog: 'Test blog post 1'
+    blog: `Test blog post ${blogPostId.value}`
   }
 }
